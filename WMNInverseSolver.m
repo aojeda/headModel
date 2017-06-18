28badb57bf6ff4c0de28f1112266bf918ec8abba
Alejandro Ojeda <ale@localhost.localdomain>
2017-06-18 09:15:38 -0700

    Remove average reference internally

--- WMNInverseSolver.m
+++ WMNInverseSolver.m
@@ -17,8 +17,13 @@ classdef WMNInverseSolver < handle
         function obj = WMNInverseSolver(hm, W)
             if nargin < 2, W = hm.L;end
             [obj.Ny, obj.Nx] = size(hm.K);
+            % Remove from the source space the indiced that are not in the
+            % gray matter, i.e. corpus callosum
             obj.srcind = hm.atlas.colorTable~=0;
-            H = obj.stdLeadField(hm.K,5);
+            % Compute the average reference operator
+            R = eye(obj.Ny)-ones(obj.Ny)/obj.Ny;
+            % Standardize lead field to compensate for depth bias
+            H = obj.stdLeadField(R*hm.K,5);
             [U,S,V] = svd(H(:,obj.srcind)/W(obj.srcind,obj.srcind),'econ');
             obj.Ut = U';
             obj.V = W(obj.srcind,obj.srcind)\V;
@@ -37,7 +42,8 @@ classdef WMNInverseSolver < handle
         function K = stdLeadField(K, alpha)
              if nargin < 3
                 indz = [];
-            else
+             else
+                % Remove outliers due to numerical errors
                 indz = std(K)< prctile(std(K),alpha);
             end
             K = bsxfun(@rdivide,K,eps+sqrt(sum(K.^2,1)));
