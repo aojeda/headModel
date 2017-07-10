f729772cef7844d4990b77e814fe631f45fe3f4e
Alejandro Ojeda <alejo.ojeda83@gmail.com>
2017-07-10 12:24:18 -0700

    Update interface with PEB class

--- plugins/bsbl.m
+++ plugins/bsbl.m
@@ -3,16 +3,21 @@ if ~exist('ParametricEmpiricalBayes','class')
     disp('ParametricEmpiricalBayes is not installed, we will fallback to LORETA.');
     obj = invSol.loreta(hm);
 else
-    % Remove the average reference
+    % Compute the average reference operator
     Ny = size(hm.K,1);
     R = eye(Ny)-ones(Ny)/Ny;
-    hm.K = R*hm.K;
+    
+    % Configure algorithm options
     options = ParametricEmpiricalBayes.initOptions();
     options.lb.verbose = false;
     options.peb.verbose = false;
     options.peb.maxIter = 50;
     options.peb.useGPU = false;
+    
+    % Get covariance components
     [PriorCov,sqrtPriorCov,blocks] = hm2cc(hm);
-    obj = ParametricEmpiricalBayes(hm, PriorCov,sqrtPriorCov,blocks, options);
+    
+    % Create peb object
+    obj = ParametricEmpiricalBayes(R*hm.K, PriorCov,sqrtPriorCov,blocks, options);
 end
 end
