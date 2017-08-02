52b413884397b3c4ce9b330e2f74c27ed1f2031b
Alejandro Ojeda <alejo.ojeda83@gmail.com>
2017-08-02 14:10:56 -0700

    Adding interface to LORETA_PEB solver

--- plugins/loreta_peb.m
+++ plugins/loreta_peb.m
@@ -1,24 +1,9 @@
 function obj = loreta_peb(hm)
-if ~exist('ParametricEmpiricalBayes','class')
-    disp('ParametricEmpiricalBayes is not installed, we will fallback to LORETA.');
+if ~exist('LORETA_PEB','class')
+    disp('LORETA_PEB is not installed, we will fallback to LORETA.');
     obj = invSol.loreta(hm);
 else
-    % Compute the average reference operator
-    Ny = size(hm.K,1);
-    R = eye(Ny)-ones(Ny)/Ny;
-    
-    % Configure algorithm options
-    options = ParametricEmpiricalBayes.initOptions();
-    options.lb.maxTol = 1e-2;
-    options.peb.maxIter = 0;
-    options.lb.verbose = false;
-    options.peb.verbose = false;
-    options.peb.useGPU = false;
-    
-    % Get covariance components
-    [PriorCov,sqrtPriorCov,blocks] = hm2cc(hm);
-    
-    % Create peb object
-    obj = ParametricEmpiricalBayes(R*hm.K, PriorCov,sqrtPriorCov,blocks, options);
+    % Create LORETA_PEB solver object
+    obj = LORETA_PEB(hm);
 end
 end
