9bb9883d239f87586d349dd657ae134805b31467
Alejandro Ojeda <alejo.ojeda83@gmail.com>
2017-08-02 12:59:33 -0700

    Adding interface to bsbl solver

--- plugins/bsbl.m
+++ plugins/bsbl.m
@@ -1,23 +1,9 @@
 function obj = bsbl(hm)
-if ~exist('ParametricEmpiricalBayes','class')
-    disp('ParametricEmpiricalBayes is not installed, we will fallback to LORETA.');
+if ~exist('BSBL','class')
+    disp('BSBL is not installed, we will fallback to LORETA.');
     obj = invSol.loreta(hm);
 else
-    % Compute the average reference operator
-    Ny = size(hm.K,1);
-    R = eye(Ny)-ones(Ny)/Ny;
-    
-    % Configure algorithm options
-    options = ParametricEmpiricalBayes.initOptions();
-    options.lb.verbose = false;
-    options.peb.verbose = false;
-    options.peb.maxIter = 50;
-    options.peb.useGPU = false;
-    
-    % Get covariance components
-    [PriorCov,sqrtPriorCov,blocks] = hm2cc(hm);
-    
-    % Create peb object
-    obj = ParametricEmpiricalBayes(R*hm.K, PriorCov,sqrtPriorCov,blocks, options);
+    % Create BSBL solver object
+    obj = BSBL(hm);
 end
 end
