7e90d5a1360c8db2e882465f6ac758490bb8d3b3
aojeda <alejo.ojeda83@gmail.com>
2018-04-23 17:33:06 -0700

    Update bsbl interface

--- plugins/bsbl.m
+++ plugins/bsbl.m
@@ -1,9 +1,19 @@
 function obj = bsbl(hm)
-if ~exist('BSBL','class')
-    disp('BSBL is not installed, we will fallback to LORETA.');
+if ~exist('BSBL2S','class')
+    disp('BSBL2S is not installed, we will fallback to LORETA.');
     obj = invSol.loreta(hm);
 else
     % Create BSBL solver object
-    obj = BSBL(hm);
+    options = BSBL2S.initOptions();
+    options.stage1.verbose = false;
+    options.stage2.verbose = false;
+    options.stage1.maxIter = 10;
+    options.stage2.maxIter = 20;
+    
+    % Get covariance components
+    [PriorCov,sqrtPriorCov,blocks] = BSBL2S.hm2cc(hm);
+    
+    % Create peb object
+    obj = BSBL2S(hm.K, PriorCov,sqrtPriorCov,blocks, options);
 end
 end
