9f5ecf6759f42093956af039fe0412d1974043d2
aojeda <alejo.ojeda83@gmail.com>
2018-09-12 19:04:04 -0700

    Adding EEGBrowserX

---
+++ +vis/EEGBrowserX.m
@@ -0,0 +1,88 @@
+% Defines the class EEGBrowserX for visualization of EEG inverse solutions on a realistic head model.
+%
+% Author: Alejandro Ojeda, NEATLABS/UCSD, Sep-2018
+
+classdef EEGBrowserX < vis.currentSourceViewer
+    properties
+        hAxes2
+        hTCursor
+        trial = 1;
+        EEG
+        hTrial
+        source
+    end
+    properties(GetAccess=private,Hidden)
+        hEEG;
+        hEEG2
+    end
+    methods
+        function obj = EEGBrowserX(EEG,figureTitle)
+            if nargin < 3, figureTitle = '';end
+            J = EEG.etc.src.actFull(:,:,1);
+            V = EEG.data(:,:,1);
+            hm = headModel.loadFromFile(EEG.etc.src.hmfile);
+            obj = obj@vis.currentSourceViewer(hm,J,V,figureTitle,false, EEG.srate, EEG.times);
+            obj.EEG = EEG;
+            obj.source = EEGSource(EEG);
+            
+            obj.hFigure.Position(3) = 750;
+            obj.hAxes.Position = [0.1300    0.4464    0.6435    0.5434];
+            obj.hAxes2 = axes('Position',[0.0915    0.1718    0.8662    0.2510]);
+            sd = 5*median(median(std(obj.EEG.data,[],2),3));
+            ytick = (obj.EEG.nbchan:-1:1)*sd;
+            V = obj.EEG.data(:,:,obj.trial) + ytick'*ones(1,obj.EEG.pnts);
+            obj.hEEG = plot(obj.hAxes2,EEG.times, V');
+            % obj.hEEG = flipud(obj.hEEG);
+            grid(obj.hAxes2,'on');
+            xlabel(obj.hAxes2,'Time (msec)');
+            ylabel(obj.hAxes2,'EEG (mV)');
+            xlim(obj.hAxes2,EEG.times([1 end]));
+            ylim(obj.hAxes2,[-ytick(end) ytick(1)+2*sd]);
+            set(obj.hAxes2,'YTickLabel',fliplr({obj.EEG.chanlocs.labels}),'YTick',fliplr(ytick))
+            hold(obj.hAxes2,'on');
+            obj.hTCursor = plot(obj.time(obj.timeCursor.Value)*[1 1],get(obj.hAxes2,'ylim'),'k-.','linewidth',0.5);
+            hold(obj.hAxes2,'off');
+            toolbarHandle = findall(obj.hFigure,'Type','uitoolbar');
+            path = fileparts(which('headModel.m'));
+            path = fullfile(path,'+vis','icons');
+            prevTrial  = imresize(imread([path filesep 'Gnome-media-skip-backward.svg.png']),[28 28]);
+            nextTrial  = imresize(imread([path filesep 'Gnome-media-skip-forward.svg.png']),[28 28]);
+            uitoggletool(toolbarHandle,'CData',prevTrial,'Separator','on','HandleVisibility','off','TooltipString','Previous trial',...
+                'onCallback',@obj.prevTrial, 'offCallback',@obj.prevTrial);
+            uitoggletool(toolbarHandle,'CData',nextTrial,'Separator','on','HandleVisibility','off','TooltipString','Next trial',...
+                'onCallback',@obj.nextTrial,'offCallback',@obj.nextTrial);
+            title(obj.hAxes2,['Trial: ' num2str(obj.trial) '/' num2str(obj.EEG.trials)]);
+        end
+        
+        %%
+        function prevTrial(obj,src,evnt)
+            obj.trial = obj.trial - 1;
+            obj.trial(obj.trial<1) = 1;
+            obj.changeTrial;
+        end
+        function nextTrial(obj,src,evnt)
+            obj.trial = obj.trial +1;
+            obj.trial(obj.trial>obj.EEG.trials) = obj.EEG.trials;
+            obj.changeTrial;
+        end
+        function changeTrial(obj)
+            obj.hFigure.Pointer = 'watch';
+            drawnow
+            obj.setJ(obj.source.g(:,:,obj.trial))
+            obj.setV(obj.EEG.data(:,:,obj.trial));
+            for k=1:obj.EEG.nbchan
+                set(obj.hEEG(k),'YData',obj.EEG.data(k,:,obj.trial) + obj.hAxes2.YTick(obj.EEG.nbchan-k+1));
+            end
+            obj.plot();
+            title(obj.hAxes2,['Trial: ' num2str(obj.trial) '/' num2str(obj.EEG.trials)]);
+            obj.hFigure.Pointer = 'arrow';
+            drawnow
+        end
+        %%
+        function plot(obj,~,~)
+            plot@vis.currentSourceViewer(obj);
+            set(obj.hTCursor,'XData',obj.time(obj.pointer)*[1 1]);
+        end
+        
+    end
+end
