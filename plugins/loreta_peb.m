59309d0655c312de0c83cd2f73dafe1606a4e6f1
aojeda <alejo.ojeda83@gmail.com>
2018-10-08 10:06:18 -0700

    Removing legacy bsbl code

--- plugins/loreta_peb.m
+++
@@ -1,9 +0,0 @@
-function obj = loreta_peb(hm)
-if ~exist('LORETA_PEB','class')
-    disp('LORETA_PEB is not installed, we will fallback to LORETA.');
-    obj = invSol.loreta(hm);
-else
-    % Create LORETA_PEB solver object
-    obj = LORETA_PEB(hm);
-end
-end
--- plugins/mrbsbl.m
+++
@@ -1,9 +0,0 @@
-function obj = mrbsbl(hm)
-if ~exist('MultiResolutionBSBL','class')
-    disp('MultiResolutionBSBL is not installed, we will fallback to LORETA.');
-    obj = invSol.loreta(hm);
-else   
-    % Create solver object
-    obj = MultiResolutionBSBL(hm);
-end
-end
